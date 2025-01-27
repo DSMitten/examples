cc90f7fc66fc95bc6e721bee1bc16bb4f0c33e81
Scott Mitten <smitten@microsoft.com>
2025-01-27 15:25:02 -0800

    Implement CleanVcpkg

--- SharedNativeHostClient/dev/scripts/CleanVcpkg.ps1
+++ SharedNativeHostClient/dev/scripts/CleanVcpkg.ps1
@@ -1,10 +1,38 @@
-$rootDir = [System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Path)
-$externalPath = Join-Path $rootDir "external"
-$vcpkgRepoPath = Join-Path $externalPath "vcpkg"
+. $PSScriptRoot/Paths.ps1
 
+$vcpkgDir = Get-VcpkgDir
 
-if (-not (Test-Path -PathType Container $vcpkgRepoPath) -and (Test-Path -PathType Container (Join-Path $vcpkgRepoPath ".git"))) {
-    Write-Host "Vcpkg has not been cloned into $vcpkgRepoPath. Please run ./dev/scripts/ensureVcpkg.ps1"
+if (-not (Test-Path -PathType Container $vcpkgDir) -and (Test-Path -PathType Container (Join-Path $vcpkgDir ".git"))) {
+    Write-Host "Vcpkg has not been cloned into $vcpkgDir. Please run ./dev/scripts/RepoChecks.ps1"
     return
 }
-Write-Verbose 'Deleting downloaded, intermediate, and built vcpkg files'
+
+Write-Host -ForegroundColor Cyan 'Deleting downloaded, intermediate, and built vcpkg files'
+$vcpkgStatusLines = & git -C $vcpkgDir status --ignored --porcelain
+$ignoreRegex = '!!\s+(?<path>[^/]+(?:/[^/]+)*)(?<dirsuffix>/?)'
+
+foreach ($line in $vcpkgStatusLines) {
+    if (-not ($line -match $ignoreRegex)) {
+        continue;
+    }
+
+    $path = Join-Path -Path $vcpkgDir -ChildPath $matches['path']
+    $dirsuffix = $matches['dirsuffix']
+    if ($dirsuffix) {
+        Write-Host "Deleting  dir: $path"
+        Remove-Item -Path $path -Recurse -Force
+    } else {
+        Write-Host "Deleting file: $path"
+        Remove-Item -Path $path -Force
+    }
+}
+
+if ($IsMacOS -or $IsLinux) {
+    $vcpkgArchivePath = Join-Path $env:HOME '.cache' 'vcpkg' 'archives'
+}
+
+if (($null -ne $vcpkgArchivePath) -and (Test-Path $vcpkgArchivePath)) {
+    Write-Host -ForegroundColor Cyan 'Deleting vcpkg archive files'
+    Write-Host "Deleting dir: $vcpkgArchivePath"
+    Remove-Item -Path $vcpkgArchivePath -Recurse -Force
+}
