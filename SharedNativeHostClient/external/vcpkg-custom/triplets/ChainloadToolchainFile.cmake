d57f59c03e58b408242f9281bf214b1c5ca7875c
Scott Mitten <smitten@microsoft.com>
2025-01-27 17:37:57 -0800

    vcpkg stuff

--- SharedNativeHostClient/external/vcpkg-custom/triplets/ChainloadToolchainFile.cmake
+++ SharedNativeHostClient/external/vcpkg-custom/triplets/ChainloadToolchainFile.cmake
@@ -3,16 +3,59 @@
 
 # Template file for the compiler paths for VCPKG to use
 # Build tools 'EnableCompiler' step reads this template and replace the placeholders with the compiler paths before invoking vcpkg install
-set(CMAKE_C_COMPILER "/usr/local/opt/llvm@18/bin/clang")
-set(CMAKE_CXX_COMPILER "/usr/local/opt/llvm@18/bin/clang++")
+#set(CMAKE_C_COMPILER "/usr/local/opt/llvm@18/bin/clang")
+#set(CMAKE_CXX_COMPILER "/usr/local/opt/llvm@18/bin/clang++")
 
-if ("${CMAKE_SYSTEM_NAME}" STREQUAL "Darwin")
-    set(CMAKE_OBJC_COMPILER "/usr/local/opt/llvm@18/bin/clang")
-    set(CMAKE_OBJCXX_COMPILER "/usr/local/opt/llvm@18/bin/clang++")
+#if ("${CMAKE_SYSTEM_NAME}" STREQUAL "Darwin")
+#    set(CMAKE_OBJC_COMPILER "/usr/local/opt/llvm@18/bin/clang")
+#    set(CMAKE_OBJCXX_COMPILER "/usr/local/opt/llvm@18/bin/clang++")
 
     # Make sure the C++ standard library files we link against are from the llvm we installed, not
     # the default ones installed with XCode
-    link_directories(BEFORE "/usr/local/opt/llvm@18/lib/c++")
+#    link_directories(BEFORE "/usr/local/opt/llvm@18/lib/c++")
+#endif()
+
+# Custom toolchain file that is used in place of the standard vcpkg toolchain
+
+function (set_compile_settings)
+
+    # This runs before the project command, so the CMAKE_<LANG>_COMPILER_ID variables have not been set
+    # So instead we'll look at the compiler executable name to find out what compiler is being used
+    cmake_path(GET CMAKE_C_COMPILER STEM compiler)
+
+    if (("${compiler}" STREQUAL "clang-cl") OR ("${compiler}" STREQUAL "clang"))
+        set(compiler_clang TRUE)
+    endif()
+
+endfunction()
+
+if (TRUE)
+    set(CMAKE_C_COMPILER "/usr/local/opt/llvm@18/bin/clang")
+    set(CMAKE_CXX_COMPILER "/usr/local/opt/llvm@18/bin/clang++")
+
+    if ("${CMAKE_SYSTEM_NAME}" STREQUAL "Darwin")
+        set(CMAKE_OBJC_COMPILER "/usr/local/opt/llvm@18/bin/clang")
+        set(CMAKE_OBJCXX_COMPILER "/usr/local/opt/llvm@18/bin/clang++")
+
+        function(set_llvm_lib_path)
+            cmake_path(GET CMAKE_OBJCXX_COMPILER PARENT_PATH llvm_bin_path)
+            cmake_path(GET llvm_bin_path PARENT_PATH llvm_path)
+            set(llvm_lib_path ${llvm_path}/lib)
+            link_directories(BEFORE ${llvm_lib_path})
+        endfunction()
+
+        # Make sure the C++ standard library files we link against are from the llvm we installed, not
+        # the default ones installed with XCode
+        set_llvm_lib_path()
+        add_link_options(LINKER:-lunwind)
+
+        # cmake can add libraries multiple times in link commands. It adds direct dependencies and then appends transitive dependencies.
+        # linking on mac produces 'ignoring duplicate library' warnings, which we don't, so suppress it
+        add_link_options(LINKER:-no_warn_duplicate_libraries)
+
+    endif()
+
+    set_compile_settings()
 endif()
 
 
--- SharedNativeHostClient/external/vcpkg-custom/triplets/x64-osx.cmake
+++ SharedNativeHostClient/external/vcpkg-custom/triplets/x64-osx.cmake
@@ -8,4 +8,24 @@ set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
 set(VCPKG_OSX_ARCHITECTURES x86_64)
 
+# -----------------------------------------------------------------------------
+# Our custom settings
+# -----------------------------------------------------------------------------
+
+# Default to building vcpkg ports with hidden visibility so they work better with project targets
+# which are also built with hidden visibility. Mixing visibilities can result in linker warnings
+# such as 'ld: warning: direct access in function' ... means the weak symbol cannot be overridden at runtime.
+
+# Some vcpkg ports can't build successfully with hidden visibility, so those will remain default
+if (NOT PORT MATCHES "openssl")
+    # Set default visibility to hidden for vcpkg target
+    set(VCPKG_C_FLAGS "${VCPKG_C_FLAGS} -fvisibility=hidden -fvisibility-inlines-hidden")
+    set(VCPKG_CXX_FLAGS "${VCPKG_CXX_FLAGS} -fvisibility=hidden -fvisibility-inlines-hidden")
+
+    # The following variables don't do anything but hopefully will in the future
+    # vpkcg issue https://github.com/microsoft/vcpkg/issues/27099 has been opened to track adding support for setting objective-c compile flags
+    set(VCPKG_OBJC_FLAGS "${VCPKG_OBJC_FLAGS} -fvisibility=hidden -fvisibility-inlines-hidden")
+    set(VCPKG_OBJCXX_FLAGS "${VCPKG_OBJCXX_FLAGS} -fvisibility=hidden -fvisibility-inlines-hidden")
+endif()
+
 set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE ${CMAKE_CURRENT_LIST_DIR}/ChainloadToolchainFile.cmake)
