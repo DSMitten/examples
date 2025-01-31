9fae70805f6ee9155bfcc2676efa81252731aa41
Scott Mitten <smitten@microsoft.com>
2025-01-30 17:44:28 -0800

    cleanup

--- SharedNativeHostClient/dev/src/clientApp/startupSteps/WebHostSystemStartupStep.cpp
+++ SharedNativeHostClient/dev/src/clientApp/startupSteps/WebHostSystemStartupStep.cpp
@@ -21,27 +21,6 @@ using namespace Microsoft::NativeHost;
 using namespace Microsoft::NativeHost::Startup;
 using namespace Microsoft::NativeHost::WindowManagement;
 
-[[nodiscard]] std::unique_ptr<WebHostSystemStartupOptions> GetStartupOptions()
-{
-    const std::filesystem::path userDataDir = GetAppUserDataDirectoryPath();
-
-#if defined(PLATFORM_WIN)
-    const xstring userDataDirString = userDataDir.wstring();
-    const std::optional<zxstring_view> browserExecutableFolder = std::nullopt;
-#elif defined(PLATFORM_MAC)
-    const xstring userDataDirString = userDataDir.string();
-    const std::optional<zxstring_view> browserExecutableFolder = WEBVIEW2_APP_PATH;
-#endif
-
-    return std::make_unique<WebHostSystemStartupOptions>(
-        c_AppName,                                // appName
-        userDataDirString,                        // userDataFolder
-        browserExecutableFolder,                  // browserExecutableFolder
-        std::nullopt,                             // iconResourceId
-        Interop::GetTasksLibraryThreadingConfig() // spThreadingConfig
-    );
-}
-
 [[nodiscard]] std::string_view WebHostSystemStartupStep::GetName() const
 {
     return c_Name;
