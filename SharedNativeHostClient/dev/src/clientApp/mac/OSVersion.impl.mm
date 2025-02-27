9c9bd9b8123d987befd4b9217c635bb720ba9def
Scott Mitten <smitten@microsoft.com>
2025-02-26 16:47:27 -0800

    Mac implementation

---
+++ SharedNativeHostClient/dev/src/clientApp/mac/OSVersion.impl.mm
@@ -0,0 +1,47 @@
+// Matching header
+#include "../OSVersion.impl.h"
+
+// Vcpkg headers
+#include <nativehost/misc-errors-api/Errors.h>
+#include <nativehost/misc-objc/mac/ObjCConversions.h>
+
+// Platform headers
+#import <Cocoa/Cocoa.h>
+#import <Foundation/Foundation.h>
+
+// STL headers
+#include <functional>
+#include <string>
+
+using namespace ClientApp::Interop;
+using namespace Microsoft::NativeHost;
+using namespace Microsoft::NativeHost::Interop;
+
+namespace {
+
+    std::wstring GetOSVersionDescription()
+    {
+        NSProcessInfo *processInfo = [NSProcessInfo processInfo];
+        return ToString(processInfo.operatingSystemVersionString);
+    }
+
+    std::wstring GetOSVersionNumber()
+    {
+        NSDictionary *systemVersion = [NSDictionary dictionaryWithContentsOfFile:@"/System/Library/CoreServices/SystemVersion.plist"];
+        return ToString(systemVersion[@"ProductBuildVersion"]);
+    }
+
+}
+[[nodiscard]] Future<void> OSVersionImpl::GetOSVersionInformation(std::function<void(ValueOrConstRef<OSVersionInformation>)> writeReturnValue)
+{
+    xstring windowsVersionName = GetOSVersionDescription();
+    xstring windowsVersion = GetOSVersionNumber();
+
+    OSVersionInformation versionInfo
+    {
+        .description = windowsVersionName,
+        .version = windowsVersion
+    };
+    writeReturnValue(versionInfo);
+    return MakePresetFuture();
+}
