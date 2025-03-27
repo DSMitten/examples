3e6a705ff7fff50e1628ad5e162c81c06e578ce7
Scott Mitten <smitten@microsoft.com>
2025-03-27 14:50:34 -0700

    remove dead code

--- SharedNativeHostClient/dev/src/install/FixupBundle.cmake
+++
@@ -1,6 +0,0 @@
-include(BundleUtilities)
-
-set(BUNDLE_PATH ${CMAKE_INSTALL_PREFIX}/bin/ClientApp.app/Contents/MacOS/ClientApp${CMAKE_EXECUTABLE_SUFFIX})
-message(STATUS "BUNDLE_PATH: ${BUNDLE_PATH}")
-
-fixup_bundle("${BUNDLE_PATH}" "" "")
